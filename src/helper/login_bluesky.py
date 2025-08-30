b3fe43a1a1e54f1090c27e69464d6d958de9d598
cosimameyer <cosima.meyer@gmail.com>
2025-08-30 06:16:11 +0200

    add typehints

--- src/helper/login_bluesky.py
+++ src/helper/login_bluesky.py
@@ -1,20 +1,37 @@
-"""Module to login to Bluesky"""
+"""Module to log into Bluesky."""
 
 import logging
+from typing import Dict, Any
+
 from atproto import Client
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-def login_bluesky(config_dict):
+
+def login_bluesky(config_dict: Dict[str, Any]) -> Client:
+    """
+    Log in to Bluesky and return the client instance.
+
+    Args:
+        config_dict: A dictionary containing Bluesky login credentials:
+            - username: str
+            - password: str
+
+    Returns:
+        A logged-in Bluesky `Client` instance.
+    """
     logger.info(
-        ' > Logging in as %s with password <TRUNCATED>',
-        config_dict['username']
+        " > Logging in as %s with password <TRUNCATED>",
+        config_dict["username"],
     )
+
     client = Client()
     client.login(
-        config_dict.get('username', ''), config_dict.get('password', '')
+        config_dict.get("username", ""),
+        config_dict.get("password", ""),
     )
-    logger.info(' > Successfully logged in')
+
+    logger.info(" > Successfully logged in")
 
     return client
