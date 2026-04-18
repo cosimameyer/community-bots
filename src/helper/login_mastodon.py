a159903b735c7cedb4a88ad1e2e32d85db1da80f
cosimameyer <cosima.meyer@gmail.com>
2026-04-18 08:24:56 +0200

    fix: Pitfalls in login_mastodon

--- src/helper/login_mastodon.py
+++ src/helper/login_mastodon.py
@@ -1,7 +1,7 @@
 """Module to log into Mastodon."""
 
 import logging
-from typing import TypedDict, Tuple
+from typing import TypedDict
 
 from mastodon import Mastodon
 
@@ -10,14 +10,11 @@ logger = logging.getLogger(__name__)
 
 class MastodonConfig(TypedDict):
     """Typed configuration for Mastodon login."""
-    client_name: str
     api_base_url: str
     access_token: str
-    username: str
-    password: str
 
 
-def login_mastodon(config_dict: MastodonConfig) -> Tuple[object, Mastodon]:
+def login_mastodon(config_dict: MastodonConfig) -> tuple[dict, Mastodon]:
     """
     Log in to Mastodon and return the account and client.
 
@@ -29,28 +26,15 @@ def login_mastodon(config_dict: MastodonConfig) -> Tuple[object, Mastodon]:
             - account: The Mastodon account object.
             - client: The Mastodon client instance.
     """
-    client_id, client_secret = Mastodon.create_app(
-        config_dict["client_name"],
-        api_base_url=config_dict["api_base_url"],
-    )
-
     client = Mastodon(
-        client_id=client_id,
         access_token=config_dict["access_token"],
-        client_secret=client_secret,
         api_base_url=config_dict["api_base_url"],
     )
-    logger.info(
-        " > Logging in as %s with password <TRUNCATED>",
-        config_dict["username"],
-    )
 
-    client.log_in(
-        config_dict["username"],
-        config_dict["password"],
-    )
+    logger.info(" > Logging in as access token holder on %s", config_dict["api_base_url"])
+
     account = client.me()
 
-    logger.info(" > Successfully logged in")
+    logger.info(" > Successfully logged in as @%s", account["username"])
 
     return account, client
